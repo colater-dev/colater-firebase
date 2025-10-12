2060e493cb85103bebb32d548745ac314aa28526
Gyan Lakhwani <gyanlakhwani@gmail.com>
2025-10-12 11:08:22 +0000

    Try fixing this error: `Console Error: FirebaseError: Missing or insuffi

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -85,8 +85,6 @@ export function useCollection<T = any>(
         setIsLoading(false);
       },
       (error: FirestoreError) => {
-        console.error("Firestore subscription error in useCollection:", error);
-
         // This logic extracts the path from either a ref or a query
         const path: string =
           memoizedTargetRefOrQuery.type === 'collection'
