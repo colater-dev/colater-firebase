013dbb6536ea2e154b052ed8cdcd4ab37a12def5
Gyan Lakhwani <gyanlakhwani@gmail.com>
2025-11-23 00:56:01 +0530

    fix crash

--- src/features/brands/components/index.ts
+++ src/features/brands/components/index.ts
@@ -2,5 +2,5 @@ export * from './brand-header';
 export * from './brand-identity-header';
 export * from './brand-identity-card';
 export * from './brand-applications';
+export * from './taglines-list';
 export * from './logo-feedback-form';
-export * from './brand-applications';
